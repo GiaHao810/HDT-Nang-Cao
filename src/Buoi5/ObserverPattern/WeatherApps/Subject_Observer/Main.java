de2690c3809dd4a8d39be60f76e908c1e1d6f849
GiaHao810 <giahaobui810@gmail.com>
2022-07-20 09:57:58 +0700

    Subject - Observer Solution

--- src/Buoi5/ObserverPattern/WeatherApps/Subject_Observer/Main.java
+++ src/Buoi5/ObserverPattern/WeatherApps/Subject_Observer/Main.java
@@ -2,15 +2,22 @@ package Buoi5.ObserverPattern.WeatherApps.Subject_Observer;
 
 public class Main {
     public static void main(String[] args) {
-
         SimpleSubject simpleSubject = new SimpleSubject();
 
         SimpleObserver simpleObserver = new SimpleObserver(simpleSubject);
 
-        simpleSubject.registerObsever(simpleObserver);
-
+        System.out.println("Lan 01");
         simpleSubject.setValue(10);
-
         simpleSubject.removeObserver(simpleObserver);
+
+        System.out.println("Lan 02");
+        SimpleObserver simpleObserver_1 = new SimpleObserver(simpleSubject);
+        simpleSubject.setValue(20);
+        simpleSubject.removeObserver(simpleObserver_1);
+
+        System.out.println("Lan 03");
+        SimpleObserver simpleObserver_2 = new SimpleObserver(simpleSubject);
+        simpleSubject.setValue(30);
+        simpleSubject.removeObserver(simpleObserver_2);
     }
 }
